bbd7024cb4162abc2c5586208113d1ec69336cc9
“shubham71099” <shubham71099@gmail.com>
2024-07-19 20:45:53 +0530

    updated form

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("splash-screen").style.display = "none";
-        document.getElementById("main-content").style.display = "block";
-    }, 1000);
-});
